d1427db217e21572c9b0f303c5dfb3feb7f83031
Marty Alchin <marty@martyalchin.com>
2012-12-14 22:09:54 -0800

    Add a List field to reference multiple related structures

--- steel/fields/compound.py
+++ steel/fields/compound.py
@@ -2,7 +2,7 @@ from gettext import gettext as _
 
 from steel.fields import Field
 
-__all__ = ['Object']
+__all__ = ['Object', 'List']
 
 
 class Object(Field):
@@ -17,3 +17,29 @@ class Object(Field):
     def decode(self, value):
         value = self.structure.loads(value)
         return super(Object, self).decode(value)
+
+
+class List(Field):
+    def __init__(self, field, *args, size, **kwargs):
+        self.field = field
+        self.length = size
+        super(List, self).__init__(*args, size=field.size * size, **kwargs)
+
+    def encode(self, value):
+        length = len(value)
+        if length > self.length:
+            raise ValueError(_('Too many %s objects to encode') % self.field.__class__.__name__)
+        elif length < self.length:
+            if self.field.has_default():
+                value.extend(self.field.default for i in range(self.length - length))
+            else:
+                raise ValueError(_('Not enough %s objects to encode') % self.field.__class__.__name__)
+
+        return b''.join(self.field.encode(v) for v in value)
+
+    def decode(self, value):
+        output = []
+        for i in range(self.length):
+            output.append(value[self.field.size * i:self.field.size * (i + 1)])
+
+        return [self.field.decode(v) for v in output]
--- tests/fields/compound.py
+++ tests/fields/compound.py
@@ -34,3 +34,29 @@ class ObjectTests(unittest.TestCase):
 
         self.assertEqual(value.field1, b'abc')
         self.assertEqual(value.field2, b'def')
+
+
+class ListTests(unittest.TestCase):
+    def setUp(self):
+        self.field = steel.List(steel.Integer(size=1), size=4)
+
+    def test_encode(self):
+        value = self.field.encode([1, 2, 3, 4])
+        self.assertEqual(value, b'\x01\x02\x03\x04')
+
+    def test_decode(self):
+        value = self.field.decode(b'\x01\x02\x03\x04')
+        self.assertEqual(value, [1, 2, 3, 4])
+
+    def test_encode_too_few(self):
+        with self.assertRaises(ValueError):
+            self.field.encode([1, 2, 3])
+
+    def test_encode_too_many(self):
+        with self.assertRaises(ValueError):
+            self.field.encode([1, 2, 3, 4, 5, 6, 7])
+
+    def test_encode_defaults(self):
+        field = steel.List(steel.Integer(size=1, default=0), size=4)
+        value = field.encode([1, 2, 3])
+        self.assertEqual(value, b'\x01\x02\x03\x00')
